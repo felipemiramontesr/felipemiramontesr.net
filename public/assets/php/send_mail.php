096ae2d28f8d7d7f6dbdb6a787f640304f1f3333
felipemiramontesr <felipemiramontesr@gmail.com>
2026-02-04 14:22:22 -0600

    feat(email): update subject line format to include site branding

--- public/assets/php/send_mail.php
+++ public/assets/php/send_mail.php
@@ -54,7 +54,7 @@ try {
         $config['from_email'],        // From (Authenticated Account)
         $config['from_name'],         // From Name
         $config['from_email'], // To (Same as From, as requested)
-        $subject . " [Web Contact]",  // Subject
+        '"' . $subject . '" - Contact from felipemiramontesr.net',  // Subject
         $body,                        // Body (HTML)
         $email                        // Reply-To (The visitor)
     );
